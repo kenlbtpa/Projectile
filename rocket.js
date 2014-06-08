b3695621b4e943ed9f0a9b4e4b11a8674ee3ea89
Kenneth L <kl1248@nyu.edu>
2014-06-08 18:26:37 -0400

    Perspective On Projectile

--- rocket.js
+++ rocket.js
@@ -31,6 +31,9 @@ $('#stop').click(function(){
 
 function animate(frame)
 {
+
+	// console.log(ui.zoom)
+
 	animTime =  new Date().getTime() - animTimeStart; 
 
 	var iVelocity = Number($('#ivelocity').val()); 
@@ -46,7 +49,6 @@ function animate(frame)
 
 	acceleration = 0; 
 	timeElapsed = stopTimeElapsed ? timeElapsed : ( storeTime +  animTime ); 
-	console.log(timeElapsed, storeTime)
 	$('#time').val( timeElapsed / 700 )
 	var deltaY = ( ( yTravelRate* (timeElapsed / 700) ) + ( 0.5 * ( acceleration + gravity ) * Math.pow(timeElapsed/700, 2) ) ); 
 	ball.setY( (startP.y - deltaY) ); 
@@ -85,10 +87,7 @@ function showPath()
 		var yValueAt = startP.y - ( (yTravelRate * i ) + (0.5 * (acceleration + gravity) * Math.pow(i,2) ) ); 
 		points.push( xValueAt, yValueAt ); 
 	}
-	// points.pop(); 
-	// console.log(points)
 	quadLine.points( points ); 
-	// quadLine.scale({x:.2,y:.2}); 
 	curveLayer.add(quadLine)
 	curveLayer.draw(); 
 
@@ -142,33 +141,43 @@ function showPath()
 		radius:5,
 		fill:"white",
 		stroke:'1',
-		id:'topMarker'
+		id:'finalMarker'
 		})
 
-		var tooltip = new Opentip(
-			"#container",
-			"Tooltip is Working", 
-			{
-				style: "glass",
-				showOn: null
-			}
-		); 
+		// var minimizedTip = "<div class='explaintrigger smalltip minimized'>Explanation</div>"; 
+		// var explainationTip = "<div class='explaintip'>Explaination</div>"; 
+		// var tooltip = new Opentip(
+		// 	"#container",
+		// 	"Tooltip is Working<hr style='margin-top:5px;margin-bottom:15px;'/>"+minimizedTip+explainationTip , 
+		// 	{
+		// 		// escapeContent:true,
+		// 		style: "glass",
+		// 		showOn: null,
+		// 		hideTrigger:'closeButton'
+		// 	}
+		// ); 
 
-		tooltip.hide(); 
+		// tooltip.hide(); 
 
-		finalPointCircle.on('click',function(){
-			tooltip.content = "Well Hello is Tooltip Working?"
-			tooltip.show(); 
-		}); 
+		// finalPointCircle.on('click',function(){
+		// 	tooltip.content = "Well Hello is Tooltip Working? Top"
+		// 	tooltip.show(); 
+		// }); 
 
-		finalPointCircle.on('mouseout',function(){
-			tooltip.content = "Well Hello is Tooltip Working?"
-			tooltip.hide();
-		}); 
+		// finalPointCircle.on('mouseout',function(){
+		// 	tooltip.content = "Well Hello is Tooltip Working?"
+		// 	tooltip.hide();
+		// }); 
 
-
+		// $('#container').click(function(){
+		// 	tooltip.show(); 
+		// 	$('.explaintrigger').click(function(){
+		// 		this.innerHTML = "Explaination\nWAHAHA\nMoreWords"; 	
+		// 	})
+		// })
 		markerLayer.add(finalPointCircle); 
 	}
+
 	markerLayer.draw(); 
 
 	return { x:finalPoint.x, y:topPoint.y }
@@ -226,7 +235,6 @@ var xTravelRate = aVelocity * Math.cos( angle * Math.PI / 180 ) ;
 var yTravelRate = aVelocity * Math.sin( angle * Math.PI / 180 ) ; 
 
 console.log( getSolutionToTimeProblem() ); 
-// console.log(  )
 
 })
 
@@ -251,53 +259,25 @@ var ui = {
 	mouseWheelZoom: function(event){
 		event.preventDefault(); 
 		var evt = event.originalEvent; 
-		// wheel = evt.wheelData/120; 
 		var zoom = ( ui.zoom - ( evt.wheelDelta/120 < 0 ? 0.05 : -0.05) ); 
-		// console.log(zoom)
 		ui.zoom = zoom; 
-		// console.log(zoom)
 		var newScale = ui.zoom * zoom; 
-		// console.log(evt.wheelDelta)
-
-		// ball.scale({x:newScale,y:newScale}); 
-		// ball.offsetY( -( (ball.getRadius()*(1-newScale) )/newScale ) ); 
-
-		// quadLine.setX( quadLine.getX()/newScale ); 
-		// quadLine.setY( quadLine.getY()/newScale ); 
 		quadLine.scale({x:newScale,y:newScale})
 		var x1 = quadLine.points()[0]
 		var y1 = quadLine.points()[1]
-		
-		// curveLayer.setX(30)
-
 		quadLine.offsetX( -(x1/newScale - x1) ); 
 		quadLine.offsetY( -(y1/newScale - y1) ); 
-
-		// console.log(  {x:x1-(x1/newScale - x1),y:y1-(y1/newScale - y1)}  )
-
-		// console.log(x1);
-
-		// curveLayer.setX( ball.getX() - x1/newScale )
-
-		// console.log(curveLayer)
-
 		curveLayer.draw(); 
 	},
 	zoomAdjust: function(event)
 	{
-		// console.log(ui.zoom)
 
-
-		// console.log(layer)
+		var perspective = {stage.getWidth()/2, y:ground.getY()}; 
 
 		var newScale = ui.zoom;  
-		console.log(newScale)
 		layer.scale({x:newScale,y:newScale}); 
 		layer.offsetX( -(ball.getX()/newScale - ball.getX()) ); 
 		layer.offsetY( -(ball.getY()/newScale - ball.getY()) ); 
-
-		console.log(layer.offsetX()); 
-
 		layer.draw(); 
 
 		quadLine.scale({x:newScale,y:newScale})
@@ -307,12 +287,24 @@ var ui = {
 		quadLine.offsetX( -(x1/newScale - x1) ); 
 		quadLine.offsetY( -(y1/newScale - y1) ); 
 
+		// markerLayer.scale({x:.5,y:.5})
+		// // var x1 = quadLine.points()[0]
+		// // var y1 = quadLine.points()[1]
+
+		// markerLayer.offsetX( -(x1/newScale - x1) ); 
+		// markerLayer.offsetY( -(y1/newScale - y1) ); 
+		markerLayer.draw(); 
 		curveLayer.draw(); 
 	}
 }
 
-
-$('#container').on('mousewheel', ui.zoomAdjust); 
+var cameraControl = true; 
+$('#container').on('mousewheel', function(event){
+	if(cameraControl){
+		ui.mouseWheelZoom(event); 
+		ui.zoomAdjust(event); 
+	}
+}); 
 
 $('#bigger').click(function(){
 	ball.scale({x:.2,y:.2}); 
@@ -322,23 +314,12 @@ $('#bigger').click(function(){
 }); 
 
 $('#rotate').click(function(){	
-	// var cannonOrigin = {"x":cannon.getX(),"y":cannon.getY()}; 
-	// cannon.setX(startP.x);
-	// cannon.setY(startP.y) 
-	// cannon.rotate(30); 
-	// console.log(cannonOrigin)
-	// cannon.offSet([cannonOrigin.x,cannonOrigin.y])
-	// cannon.setX(cannonOrigin.x); 
-	// cannon.setY(cannonOrigin.y); 
 	cannon.rotation(-45+(26.5))
 	layer.draw(); 
 })
 
 $('#container').mousemove(function(e){
 	return;
-	// console.log("asdf")
-	// console.log(cannon.getX())
-	// console.log(e)
 	x0=ball.getX(); 
 	y0=ball.getY(); 
 	c=Math.pow( Math.pow( e.pageX-x0,2) + Math.pow( e.pageY-y0,2)  ,0.5)
@@ -354,9 +335,6 @@ $('#container').mousemove(function(e){
 	}
 	$('#angle').val( (rotation-15)*-1 ); 
 	showPath(); 
-	// console.log(c); 
-	// console.log(  -1*(e.pageY-cannon.getY()) ); 
-	// console.log( Math.asin( -1*(e.pageY-cannon.getY())/c ) * 180 / Math.PI  ); 
 	layer.draw(); 
 }); 
 
@@ -374,3 +352,128 @@ if( maxBorder > ($('#container').width()*.8) )
 	ui.zoomAdjust(); 
 
 }
+
+var zoomRectProp = {width:75,height:35}
+var zoomRect = new Kinetic.Rect({
+	x:stage.getWidth()-(zoomRectProp.width+20),
+	y:10,
+	fill:'gray',
+	width:zoomRectProp.width,
+	height:zoomRectProp.height,
+	cornerRadius:7,
+});
+
+var text = ui.zoom; 
+text = text.toString(); 
+text = text.length > 4 ? text.substring( 0, 4 ) : text; 
+
+var zoomText = new Kinetic.Text({
+	x:zoomRect.getX(),
+	y:zoomRect.getY(),
+	text:text+"x",
+	fill:'white',
+	fontSize:20,
+	offset:{x:-12,y:-8}
+})
+uiLayer.add(zoomRect); 
+uiLayer.add(zoomText); 
+
+uiLayer.draw();
+
+
+$('input[type="checkbox"').click(function(){
+	cameraControl = this.checked; 
+});
+
+var angle = Number($('#angle').val()); 
+var aVelocity = Number($('#avelocity').val());
+var deltaX = aVelocity * Math.cos( angle * Math.PI / 180 ) ; 
+var deltaY = aVelocity * Math.sin( angle * Math.PI / 180 ) ; 
+var pointerPosition = {x: ball.getX() + deltaX, y: ball.getY() - deltaY}; 
+var pointerLine = new Kinetic.Line({
+	strokeWidth:'1',
+	stroke:'black',
+	lineCap:'round',
+	points:[ball.getX(),ball.getY(),pointerPosition.x,pointerPosition.y] 
+})
+// var triangle = new Kinetic.RegularPolygon({
+// 	x:pointerPosition.x,
+// 	y:pointerPosition.y,
+// 	radius:8,
+// 	sides:3,
+// 	fill:'black',
+// 	strokeWidth:1,
+// 	stroke:'black'
+// }); 
+
+var circleController = new Kinetic.Circle({
+	x:pointerPosition.x,
+	y:pointerPosition.y,
+	radius:8,
+	fill:'white',
+	stroke:'black',
+	strokeWidth:1,
+	draggable: true,
+	dragDistance: 1,
+	dragBoundFunc:function(pos){
+		// console.log(pos)
+		var x = ball.getX();
+        var y = ball.getY();
+        var radius = 120;
+        var scale = radius / Math.sqrt(Math.pow(pos.x - x, 2) + Math.pow(pos.y - y, 2));
+        if(scale < 1){
+          var yBound = Math.round((pos.y - y) * scale + y); 
+          var xBound = Math.round((pos.x - x) * scale + x);
+          return {
+            y: pos.y > ball.getY() ? ball.getY() : yBound,
+            x: pos.x < ball.getX() ? ball.getX() : xBound
+          };
+      	}
+        else
+          return { x: pos.x < ball.getX() ? ball.getX() : pos.x , y: pos.y > ball.getY() ? ball.getY() : pos.y };
+	}
+}); 
+
+circleController.on('dragmove',function(){
+	// console.log(circleController.getX(),circleController.getY())
+	$('#angle').val( Math.atan2( (ball.getY() - circleController.getY()) , circleController.getX()-ball.getX() ) * (180/Math.PI) )//.trigger('change')
+	pointerLine.points( [ pointerLine.points()[0], pointerLine.points()[1], circleController.getX(), circleController.getY() ] ); 
+	var distance = Math.sqrt(Math.pow(circleController.getY() - ball.getY(),2) + Math.pow(circleController.getX() - ball.getX(),2)); 
+	// distance *= (1/ui.zoom)
+	$('#avelocity').val(distance); 
+	showPath(); 
+
+});
+
+$('#angle').change(function(){
+	var angle = Number($('#angle').val()); 
+	var aVelocity = Number($('#avelocity').val());
+	var deltaX = aVelocity * Math.cos( angle * Math.PI / 180 ) ; 
+	var deltaY = aVelocity * Math.sin( angle * Math.PI / 180 ) ; 
+	var pointerPosition = {x: ball.getX() + deltaX, y: ball.getY() - deltaY}; 
+	circleController.x( pointerPosition.x ); 
+	circleController.y( pointerPosition.y ); 
+	pointerLine.points( [ pointerLine.points()[0], pointerLine.points()[1], circleController.getX(), circleController.getY() ] ); 
+	arrowLayer.draw(); 
+	showPath(); 
+}); 
+
+$('#avelocity').change(function(){
+	var angle = Number($('#angle').val()); 
+	var aVelocity = Number($('#avelocity').val());
+	var deltaX = aVelocity * Math.cos( angle * Math.PI / 180 ) ; 
+	var deltaY = aVelocity * Math.sin( angle * Math.PI / 180 ) ; 
+	var pointerPosition = {x: ball.getX() + deltaX, y: ball.getY() - deltaY}; 
+	circleController.x( pointerPosition.x ); 
+	circleController.y( pointerPosition.y ); 
+	pointerLine.points( [ pointerLine.points()[0], pointerLine.points()[1], circleController.getX(), circleController.getY() ] ); 
+	arrowLayer.draw(); 
+	showPath(); 
+})
+
+// triangle.rotate( Math.atan2( deltaX,deltaY ) * (180/Math.PI) )
+
+console.log( pointerLine.points() )
+arrowLayer.add(pointerLine);
+arrowLayer.add(circleController)
+arrowLayer.draw();  
