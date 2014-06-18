a39f929822ae1beba46684f0f202a05665a23b39
Kenneth L <kl1248@nyu.edu>
2014-06-18 01:59:10 -0400

    All Ready Except Tooltip

--- rocket.js
+++ rocket.js
@@ -1,5 +1,7 @@
 var velocity = {x:50,y:0}
 var time = isNaN($('#time').val()) || $('#time').val().trim() == "" ? 3 : $('#time').val(); 
+var data = {time:0, avelocity: 0, acceleration: 0, angle: 0, gravity: 0 }
+var andBeyond = false; 
 
 $('#velocityx').val(velocity.x);
 $('#velocityy').val(velocity.y);
@@ -25,7 +27,6 @@ $('#move').click(function(){
 }); 
 
 $('#stop').click(function(){
-	// startTime=new Date().getTime(); 
 	stopTimeElapsed=true;
 	storeTime = timeElapsed; 
 	anim.stop(); 
@@ -73,10 +74,9 @@ function animate(frame)
 	}
 	var deltaX = ( xTravelRate * (timeElapsed / 700) ) + ( 0.5 * acceleration * Math.pow(timeElapsed / 700, 2) )
 	ball.setX( (startP.x + deltaX) 	); 		
-
-	// console.log(ball.x(),ball.y())
 } 
-
+var finalPointCircle; 
+var topPointCircle; 
 function showPath()
 {
 	var iVelocity = Number($('#ivelocity').val()); 
@@ -89,11 +89,13 @@ function showPath()
 
 	/*For every 10 distance , create a 3 radius circle to show the path. Ratesepeat until path ends. */
 	var timeElapsed = getSolutionToTimeProblem(); 
+
+	if(andBeyond){
+		alert("There is no solution. Projectile seems to go on forever. Only partial path is shown.")
+	}
 	// Determine xlength traveled. 
-	acceleration=0; 
 	var deltaX = ( xTravelRate * timeElapsed ) + ( 0.5 * acceleration * Math.pow(timeElapsed, 2) ); 
 	var deltaY = (yTravelRate * (yTravelRate/9.8) ) + (0.5 * (acceleration + gravity) * Math.pow(yTravelRate/9.8,2) ); 
-	// console.log( {"yTopPoint":(startP.y+deltaY),"xTopPoint":(startP.x+deltaX) } )
 	var points = []; 
 	for(var i = 0; i < timeElapsed; i+=(timeElapsed/100) )
 	{
@@ -105,6 +107,14 @@ function showPath()
 	mainGroup.add(quadLine); 
 	mainLayer.draw(); 
 
+	/*Marker Code*/
+	if(andBeyond){
+		var displayTime = 20; 
+		var xPortion = startP.x + ( xTravelRate * displayTime ) + ( 0.5 * acceleration * Math.pow(displayTime, 2) ) 
+		var yPortion = startP.y - ( (yTravelRate * displayTime ) + (0.5 * (acceleration + gravity) * Math.pow(displayTime,2) ) )
+		// returns the boundaries so that the image could be displayed. 
+		return { x:xPortion, y:yPortion }
+	}
 	var topPointTime = (-1*yTravelRate)/(acceleration+gravity); 
 	var topPoint = {
 		x:startP.x + ( xTravelRate * topPointTime ) + ( 0.5 * acceleration * Math.pow(topPointTime, 2) ) ,
@@ -121,10 +131,13 @@ function showPath()
 		var topMarker = mainGroup.find('#topMarker')[0]
 		topMarker.setX( topPoint.x );
 		topMarker.setY( topPoint.y ); 
+		var topMarkerLine = mainGroup.find('#topMarkerLine')[0]
+		topMarkerLine.points([ topPointCircle.x() , topPointCircle.y(), topPointCircle.x(), ground.y() ]); 
+		topMarker.moveToTop(); 
 	}
 	else
 	{
-		var topPointCircle = new Kinetic.Circle({
+		topPointCircle = new Kinetic.Circle({
 		x:topPoint.x,
 		y:topPoint.y,
 		radius:5,
@@ -147,12 +160,20 @@ function showPath()
 		); 
 
 		var topPointTipContent = "Projectile landed at " + (topPointCircle.x() - ball.x()) + " units away from initial position."; 
-		var explainationTip = "<div class='hidden-explaintip'>Here is the math.<br/>asdf<br/>asdf\n\nasdf</div>"; 
+		var explainationTip = ""; 
+		if(andBeyond){
+			"<div class='hidden-explaintip'></div>"
+		}
+		else
+		{
+			explainationTip = 
+			"<div class='hidden-explaintip'>The top point of a projectile is when the the projectile begins to fall.<br />This can be seen as when the vertical velocity of the projectile reaches 0.<br /> v<sub>f</sub>=v<sub>i</sub>+a*t The final velocity is 0.<br /> 0=v<sub>i</sub>+a*t<br/> -v<sub>i</sub>=a*t<br/> -v<sub>i</sub>/a=t<br/> Now that we determine the formula to determine the time where the projectile begins to fall, lets determine the variables. -v<sub>i</sub> is the vertical velocity component of the projectile. <br/> The vertical velocity component of the projectile can be determined by<br/> vsin&Theta;=opp where v is the velocity, &Theta; is the angle, and opp is the vertical velocity component, v<sub>i</sub> <br /> </div>"}
 		topPointTip.content = 
 			topPointTipContent + topPointTip.content + "<hr style='margin-top:20px;margin-bottom:15px;'/>"
 		topPointTip.hide(); 
+		var completeTip = topPointTip.content + explainationTip; 
 
-		topPointCircle.on('click',function(){
+		topPointCircle.on('mouseup',function(){
 			topPointTip.show(); 
 			var tipDisplayPosition=
 			{
@@ -161,9 +182,8 @@ function showPath()
 			}		 
 
 			$('.explaintrigger').click(function(){
-				topPointTip.setContent(topPointTip.content + explainationTip); 
+				topPointTip.setContent(completeTip); 
 				$('#opentip-1').offset(tipDisplayPosition)
-				// $(this).parent().parent().offset(tipDisplayPosition)
 			}); 
 
 		}); 
@@ -171,17 +191,30 @@ function showPath()
 
 		mainGroup.add(topPointCircle); 
 
+		var topMarkerLine = new Kinetic.Line({
+		dash:[10,10],
+		strokeWidth:2,
+		stroke:'gray',
+		lineCap:'round',
+		opacity:0.6,
+		id:'topMarkerLine',
+		points:[ topPointCircle.x() , topPointCircle.y(), topPointCircle.x(), ground.y() ]
+		});
+		mainGroup.add(topMarkerLine); 
 	}
 
 	if(mainGroup.find('#finalMarker').length != 0)
 	{
-		var topMarker = mainGroup.find('#finalMarker')[0]
-		finalMarker.setX( topPoint.x );
-		finalMarker.setY( topPoint.y ); 
+		var finalMarker = mainGroup.find('#finalMarker')[0]
+		finalMarker.setX( finalPoint.x );
+		finalMarker.setY( finalPoint.y ); 
+		var finalMarkerLine = mainGroup.find('#finalMarkerLine')[0]; 
+		finalMarkerLine.points([ finalPointCircle.x() , finalPointCircle.y(), finalPointCircle.x(), ground.y() ])
+		finalMarker.moveToTop(); 
 	}
 	else
 	{
-		var finalPointCircle = new Kinetic.Circle({
+		finalPointCircle = new Kinetic.Circle({
 		x:finalPoint.x, 
 		y:finalPoint.y,
 		radius:5,
@@ -209,9 +242,8 @@ function showPath()
 			finalPointTipContent + finalPointTip.content + "<hr style='margin-top:20px;margin-bottom:15px;'/>"
 		finalPointTip.hide(); 
 
-		finalPointCircle.on('click',function(){
+		finalPointCircle.on('mouseup',function(){
 			finalPointTip.show(); 
-			// console.log($('#opentip-1'))
 			var tipDisplayPosition=
 			{
 			 left:$('#opentip-2').position().left,
@@ -221,40 +253,31 @@ function showPath()
 			$('.explaintrigger').click(function(){
 				finalPointTip.setContent(finalPointTip.content + explainationTip); 
 				$('#opentip-2').offset(tipDisplayPosition)
-				// $(this).parent().parent().offset(tipDisplayPosition)
 			}); 
 
 		}); 
 		/*EOF Tooltip Code*/
 
+		var finalMarkerLine = new Kinetic.Line({
+		dash:[10,10],
+		strokeWidth:2,
+		stroke:'gray',
+		lineCap:'round',
+		opacity:0.6,
+		id:'finalMarkerLine',
+		points:[ finalPointCircle.x() , finalPointCircle.y(), finalPointCircle.x(), ground.y() ]		
+		})
+		mainGroup.add(finalMarkerLine); 
+
 		mainGroup.add(finalPointCircle); 
 	}
 
-	var topMarkerLine = new Kinetic.Line({
-		dash:[10,10],
-		strokeWidth:2,
-		stroke:'gray',
-		lineCap:'round',
-		opacity:0.6,
-		points:[ topPointCircle.x() , topPointCircle.y(), topPointCircle.x(), ground.y() ]
-	});
-
-	var finalMarkerLine = new Kinetic.Line({
-		dash:[10,10],
-		strokeWidth:2,
-		stroke:'gray',
-		lineCap:'round',
-		opacity:0.6,
-		points:[ finalPointCircle.x() , finalPointCircle.y(), finalPointCircle.x(), ground.y() ]		
-	})
-
-	mainGroup.add(topMarkerLine); 
-	mainGroup.add(finalMarkerLine); 
-
 	topMarkerLine.moveToBottom(); 
 	finalMarkerLine.moveToBottom(); 
+	ball.moveToTop(); 
 
 	mainGroup.draw(); 
+	mainLayer.draw(); 
 
 	return { x:finalPoint.x, y:topPoint.y }
 }
@@ -278,16 +301,13 @@ $('#direct').click(function(){
 	
 	var deltaX = ( xTravelRate * timeNow ) + ( 0.5 * acceleration * Math.pow(timeNow, 2) )
 	ball.setX( startP.x + deltaX ); 		
-
-
-	console.log( ball.getY() )
-
 	layer.draw(); 
 });
 
 
 function getSolutionToTimeProblem()
 {
+andBeyond=false;
 var iVelocity = Number($('#ivelocity').val()); 
 var aVelocity = Number($('#avelocity').val()); 
 var acceleration = Number($('#acceleration').val()); 
@@ -296,7 +316,10 @@ var gravity = Number($('#gravity').val());
 
 var xTravelRate = aVelocity * Math.cos( angle * Math.PI / 180 ) ; 
 var yTravelRate = aVelocity * Math.sin( angle * Math.PI / 180 ) ; 
-return ( yTravelRate / (-0.5 * ( gravity )) ); 
+var solution = ( -2*yTravelRate / (( gravity + acceleration )) ); 
+if(solution<0){andBeyond=true;}
+return solution < 0 ? 60 : solution; 
+
 }
 
 $('#solution').click(function(){
@@ -348,7 +371,7 @@ var ui = {
 	},
 	zoomAdjust: function(event)
 	{
-
+		return;
 		var perspective = {x: stage.getWidth()/2, y:ground.getY()}; 
 
 		var newScale = ui.zoom;  
@@ -379,6 +402,8 @@ var ui = {
 
 function zoomLayers(newScale)
 {
+	var x1 = ball.x()
+	var y1 = ball.y()
 	mainGroup.scale({x:newScale,y:newScale}); 
 	if(newScale<1)
 	{
@@ -409,8 +434,13 @@ function zoomLayers(newScale)
 		// if( ground.width() > 9000000 ) {ground.width(9000000);restrictZoom=true;console.log("limit2");}
 		// if( ground.height() > 9000000  ) {ground.height(9000000);restrictZoom=true;console.log("limit3");}		
 	}
-
-	
+	// console.log(ball.position())
+	// wedgeBackground.offsetX( -(x1/newScale - x1) ); 
+	// wedgeBackground.offsetY( -(y1/newScale - y1) );	// ((ball.position().x+)*newScale)+mainGroup.x()
+	var newPosition = {x: ((ball.position().x-ball.radius()-10)*newScale)+mainGroup.position().x, 
+						y:((ball.position().y+ball.radius()+10)*newScale)+mainGroup.position().y }; 
+	wedgeBackground.position( newPosition )
+	shinyWedge.position(newPosition)
 	// layer.scale({x:newScale,y:newScale}); 
 	// curveLayer.scale({x:newScale,y:newScale});
 	// markerLayer.scale({x:newScale,y:newScale})
@@ -434,14 +464,17 @@ function zoomLayers(newScale)
 var cameraControl = true; 
 $('#container').on('mousewheel', function(event){
 	if(cameraControl){
-
-// 		console.log(layer.width(),stage.width(),layer.width()*newScale)
-// return;
 		var evt = event.originalEvent; 
 		var zoom = ( ui.zoom - ( evt.wheelDelta/120 < 0 ? 0.05 : -0.05) ); 
 		ui.zoom = zoom; 
 		if(ui.zoom<.01) {ui.zoom=.01;}
 		zoomLayers(ui.zoom)
+		var zoomTxt = ui.zoom; 
+		zoomTxt = zoomTxt.toString(); 
+		zoomTxt = zoomTxt.length > 4 ? zoomTxt.substring( 0, 4 ) : zoomTxt;
+		zoomTxt += "x"
+		zoomText.text(zoomTxt); 
+		uiLayer.draw() ;
 		return;
 		ui.mouseWheelZoom(event); 
 		ui.zoomAdjust(event); 
@@ -509,49 +542,6 @@ $('#container').mousemove(function(e){
 	layer.draw(); 
 }); 
 
-var borderPoints = showPath(); 
-
-var maxBorder = Math.max( borderPoints.x , borderPoints.y )
-
-if( maxBorder > ($('#container').width()*.8) )
-{
-	console.log("Expand")
-
-	var borderRatio = $('#container').width()*.8/maxBorder; 
-
-	ui.zoom = borderRatio; 
-	ui.zoomAdjust(); 
-
-}
-
-var zoomRectProp = {width:75,height:35}
-var zoomRect = new Kinetic.Rect({
-	x:stage.getWidth()-(zoomRectProp.width+20),
-	y:10,
-	fill:'gray',
-	width:zoomRectProp.width,
-	height:zoomRectProp.height,
-	cornerRadius:7,
-});
-
-var text = ui.zoom; 
-text = text.toString(); 
-text = text.length > 4 ? text.substring( 0, 4 ) : text; 
-
-var zoomText = new Kinetic.Text({
-	x:zoomRect.getX(),
-	y:zoomRect.getY(),
-	text:text+"x",
-	fill:'white',
-	fontSize:20,
-	offset:{x:-12,y:-8}
-})
-uiLayer.add(zoomRect); 
-uiLayer.add(zoomText); 
-
-uiLayer.draw();
-
-
 $('input[type="checkbox"').click(function(){
 	cameraControl = this.checked; 
 });
@@ -606,47 +596,204 @@ var circleController = new Kinetic.Circle({
 }); 
 
 circleController.on('dragmove',function(){
-	// console.log(circleController.getX(),circleController.getY())
 	$('#angle').val( Math.atan2( (ball.getY() - circleController.getY()) , circleController.getX()-ball.getX() ) * (180/Math.PI) )//.trigger('change')
 	pointerLine.points( [ pointerLine.points()[0], pointerLine.points()[1], circleController.getX(), circleController.getY() ] ); 
 	var distance = Math.sqrt(Math.pow(circleController.getY() - ball.getY(),2) + Math.pow(circleController.getX() - ball.getX(),2)); 
-	// distance *= (1/ui.zoom)
 	$('#avelocity').val(distance); 
 	showPath(); 
 
 });
 
-$('#angle').change(function(){
-	var angle = Number($('#angle').val()); 
-	var aVelocity = Number($('#avelocity').val());
-	var deltaX = aVelocity * Math.cos( angle * Math.PI / 180 ) ; 
-	var deltaY = aVelocity * Math.sin( angle * Math.PI / 180 ) ; 
-	var pointerPosition = {x: ball.getX() + deltaX, y: ball.getY() - deltaY}; 
-	circleController.x( pointerPosition.x ); 
-	circleController.y( pointerPosition.y ); 
-	pointerLine.points( [ pointerLine.points()[0], pointerLine.points()[1], circleController.getX(), circleController.getY() ] ); 
-	mainLayer.draw(); 
-	showPath(); 
+var wedgeBackground = new Kinetic.Wedge({
+    x:ball.x()-ball.radius()-10,
+    y:ball.y()+ball.radius()+10,
+    radius: 100,
+    angle: 90,
+    fillRadialGradientStartRadius: 0,
+    fillRadialGradientEndRadius: 100,
+    fillRadialGradientColorStops: [0, 'green', .33, 'yellow' , .66 , 'orange' , .99 , 'red' ],
+    fillPriority: 'radial-gradient',
+    rotation: -90,
+    opacity:0.7
+});
+
+mainLayer.add(wedgeBackground); 
+var shinyWedge = new Kinetic.Wedge({
+	x: wedgeBackground.x(),
+	y: wedgeBackground.y(),
+	radius: (wedgeBackground.radius()*(Number($('#avelocity').val())/100)), 
+	angle: 30,
+	rotation: (-Number($('#angle').val())-10), 
+	fillRadialGradientStartRadius: 0, 
+	fillRadialGradientEndRadius: 100, 
+	fillRadialGradientColorStops: [0, 'white', 1.0 , 'blue' ],
+	opacity: 0.6,
+	listening:false
 }); 
 
-$('#avelocity').change(function(){
-	var angle = Number($('#angle').val()); 
-	var aVelocity = Number($('#avelocity').val());
-	var deltaX = aVelocity * Math.cos( angle * Math.PI / 180 ) ; 
-	var deltaY = aVelocity * Math.sin( angle * Math.PI / 180 ) ; 
-	var pointerPosition = {x: ball.getX() + deltaX, y: ball.getY() - deltaY}; 
-	circleController.x( pointerPosition.x ); 
-	circleController.y( pointerPosition.y ); 
-	pointerLine.points( [ pointerLine.points()[0], pointerLine.points()[1], circleController.getX(), circleController.getY() ] ); 
-	mainLayer.draw(); 
-	showPath(); 
+var wedgeDragOn = false; 
+
+wedgeBackground.on('mousemove',function(){
+	if(shinyWedge.opacity() != 0 && wedgeBackground.opacity() != 0 )
+	{
+		var difX = stage.getPointerPosition().x - wedgeBackground.x() 
+	    var difY = wedgeBackground.y() - stage.getPointerPosition().y 
+	    shinyWedge.rotation( -(Math.atan2( difY , difX ) * (180/Math.PI)) - 10 )
+	    shinyWedge.listening(false); 
+	    $('#angle').val(-shinyWedge.rotation()-10); 
+	    showPath(); 
+		mainLayer.draw(); 
+	}
 })
+
+stage.on('mouseup', function(){wedgeDragOn=false;})
+ground.on('mousedown', function(){
+	wedgeDragOn = false; 
+	shinyWedge.opacity(0); 
+	wedgeBackground.opacity(0); 
+	shinyWedge.moveToBottom(); 
+	wedgeBackground.moveToBottom(); 
+})
+wedgeBackground.on('mousedown', function(){wedgeDragOn=true;})
+wedgeBackground.on('mouseleave', function(){
+	return;
+	wedgeDragOn=false; console.log(stage.getPointerPosition()) 
+	var difX = stage.getPointerPosition().x - shinyWedge.x(); 
+	var difY = shinyWedge.y() - stage.getPointerPosition().y; 
+	var currentRadius = shinyWedge.radius(); 
+	if(currentRadius*1.1 >= wedgeBackground.radius()){
+		shinyWedge.radius(wedgeBackground.radius()); 
+	}
+	else if(currentRadius-(wedgeBackground*0.1)  <= 0) {
+		shinyWedge.radius(0); 
+	}
+})
+
+stage.on('mousemove',function(){
+	if(wedgeDragOn){
+		var difX = stage.getPointerPosition().x - shinyWedge.x(); 
+		var difY = shinyWedge.y() - stage.getPointerPosition().y; 
+		shinyWedge.radius( Math.pow( Math.pow(difX, 2) + Math.pow(difY, 2) , .5 ) ); 
+		if(shinyWedge.radius()>= wedgeBackground.radius()){shinyWedge.radius(wedgeBackground.radius());}
+		if(difX<= 0 && difY<=0){shinyWedge.radius(0);}
+		var lengthVal = (shinyWedge.radius()/wedgeBackground.radius())*100
+		$('#avelocity').val(lengthVal); 
+		showPath(); 
+		mainLayer.draw(); 
+	}
+})
+
+background.on('mousedown',function(){
+	wedgeDragOn = false; 
+	shinyWedge.opacity(0); 
+	wedgeBackground.opacity(0); 
+	shinyWedge.moveToBottom(); 
+	wedgeBackground.moveToBottom(); 
+})
+
+mainLayer.add(shinyWedge); 
+
+ball.on('mouseup',function(){    
+	var newScale = ui.zoom; 
+	var newPosition = {x: ((ball.position().x-ball.radius()-10)*newScale)+mainGroup.position().x, 
+						y:((ball.position().y+ball.radius()+10)*newScale)+mainGroup.position().y }; 
+	shinyWedge.position(newPosition);  
+	wedgeBackground.position(newPosition); 
+    wedgeBackground.opacity(0.7)
+    shinyWedge.opacity(0.6)
+    wedgeBackground.moveToTop(); 
+    shinyWedge.moveToTop(); 
+    mainLayer.draw(); 
+})
+
+mainLayer.draw(); 
 
 // triangle.rotate( Math.atan2( deltaX,deltaY ) * (180/Math.PI) )
 
-// console.log( pointerLine.points() )
-// mainGroup.add(pointerLine);
-// mainGroup.add(circleController)
-// mainGroup.draw();  
-
+ball.moveToTop(); 
 ui.zoom = 1; 
+
+$('body').click(function(){
+	return;
+	if(shinyWedge != null) {	
+		shinyWedge.opacity(0); 
+		wedgeBackground.opacity(0); 
+		wedgeBackground.moveToBottom(); 
+    	shinyWedge.moveToBottom(); 
+		mainLayer.draw(); 
+	}
+})
+
+function adjustWedges()
+{
+	shinyWedge.rotation( (-Number($('#angle').val())-10) )
+	mainLayer.draw(); 
+}
+
+$('#angle').change(function(){
+	showPath(); 
+	adjustWedges(); 
+}); 
+
+$('#avelocity').change(function(){
+	showPath(); 
+})
+
+$('#acceleration').change(function(){
+	showPath(); 
+})
+
+$('#gravity').change(function(){
+	showPath(); 
+})
+
+/*Border Points Code*/
+
+var borderPoints = showPath(); 
+var maxBorder = Math.max( borderPoints.x , borderPoints.y )
+
+if( maxBorder > ($('#container').width()*.8) && false )
+{
+	var borderRatio = $('#container').width()*.8/maxBorder; 
+	ui.zoom = borderRatio; 
+	var newScale = ui.zoom; 
+
+	zoomLayers(newScale);
+	mainGroup.position({x:0,y:$('#container').height()*.8*borderRatio}); 
+	console.log(mainGroup.position(), mainGroup.scale() ) 
+	var newPosition = {x: ((ball.position().x-ball.radius()-10)*newScale)+mainGroup.position().x, 
+						y:((ball.position().y+ball.radius()+10)*newScale)+mainGroup.position().y };
+	wedgeBackground.position( newPosition )
+	shinyWedge.position(newPosition) 
+	mainLayer.draw();
+
+}
+
+/*EOF Border Points*/
+
+var zoomRectProp = {width:75,height:35}
+var zoomRect = new Kinetic.Rect({
+	x:stage.getWidth()-(zoomRectProp.width+20),
+	y:10,
+	fill:'gray',
+	width:zoomRectProp.width,
+	height:zoomRectProp.height,
+	cornerRadius:7,
+});
+
+var text = ui.zoom; 
+text = text.toString(); 
+text = text.length > 4 ? text.substring( 0, 4 ) : text; 
+
+var zoomText = new Kinetic.Text({
+	x:zoomRect.getX(),
+	y:zoomRect.getY(),
+	text:text+"x",
+	fill:'white',
+	fontSize:20,
+	offset:{x:-12,y:-8}	
+})
+uiLayer.add(zoomRect); 
+uiLayer.add(zoomText); 
+
+uiLayer.draw();
